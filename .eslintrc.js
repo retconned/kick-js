d7fea0d090c30e7610cf6abed01c2aa3c156eac3
retconned <retcnned@gmail.com>
2024-09-05 07:31:52 +0100

    Add eslint & prettier

---
+++ .eslintrc.js
@@ -0,0 +1,59 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  overrides: [
+    {
+      extends: [
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+      files: ["*.ts"],
+      parserOptions: {
+        project: path.join(__dirname, "tsconfig.json"),
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: path.join(__dirname, "tsconfig.json"),
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["plugin:@typescript-eslint/recommended"],
+  rules: {
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "type-imports",
+        fixStyle: "inline-type-imports",
+      },
+    ],
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-misused-promises": [
+      2,
+      {
+        checksVoidReturn: { attributes: false },
+      },
+    ],
+    "@typescript-eslint/naming-convention": [
+      "warn",
+      {
+        selector: ["parameter"],
+        leadingUnderscore: "require",
+        format: ["camelCase"],
+        modifiers: ["unused"],
+      },
+    ],
+  },
+  ignorePatterns: [
+    "temp.js",
+    "config/*",
+    "dist/*",
+    ".cache",
+    "public",
+    "node_modules",
+    "*.esm.js",
+  ],
+};
+
+module.exports = config;
