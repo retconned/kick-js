470599fa9420e1f3c3903e20e41b7cb5f8e970cf
retconned <retcnned@gmail.com>
2025-09-11 23:53:47 +0100

    fix(core): upgrades ws to 8.4.0

--- src/core/websocket.ts
+++ src/core/websocket.ts
@@ -4,23 +4,23 @@ import { URLSearchParams } from "url";
 const BASE_URL = "wss://ws-us2.pusher.com/app/32cbd69e4b950bf97679";
 
 export const createWebSocket = (chatroomId: number): WebSocket => {
-  const urlParams = new URLSearchParams({
-    protocol: "7",
-    client: "js",
-    version: "7.4.0",
-    flash: "false",
-  });
-  const url = `${BASE_URL}?${urlParams.toString()}`;
-
-  const socket = new WebSocket(url);
-
-  socket.on("open", () => {
-    const connect = JSON.stringify({
-      event: "pusher:subscribe",
-      data: { auth: "", channel: `chatrooms.${chatroomId}.v2` },
+    const urlParams = new URLSearchParams({
+        protocol: "7",
+        client: "js",
+        version: "8.4.0",
+        flash: "false",
     });
-    socket.send(connect);
-  });
+    const url = `${BASE_URL}?${urlParams.toString()}`;
 
-  return socket;
+    const socket = new WebSocket(url);
+
+    socket.on("open", () => {
+        const connect = JSON.stringify({
+            event: "pusher:subscribe",
+            data: { auth: "", channel: `chatrooms.${chatroomId}.v2` },
+        });
+        socket.send(connect);
+    });
+
+    return socket;
 };
